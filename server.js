216b802d2562bc05d776d86120b7ca325a177e56
Xiaochen Wang <ryan.wang23@hotmail.com>
2017-05-25 22:25:30 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,53 @@
+/*********************************************************************************
+*  WEB322 –Assignment02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ______________________ Student ID: ______________ Date: ________________
+*
+*  Online (Heroku) Link: https://secret-brushlands-90591.herokuapp.com/
+*
+********************************************************************************/ 
+// var express = require("express");
+// var app = express();
+// var path = require("path");
+
+// var HTTP_PORT = process.env.PORT || 8080;
+
+// // call this function after the http server starts listening for requests
+// function onHttpStart() {
+//   console.log("Express http server listening on: " + HTTP_PORT);
+// }
+
+// // setup a 'route' to listen on the default url path (http://localhost)
+// app.get("/", function(req,res){
+//    res.sendFile(path.join(__dirname + "/views/home.html"));
+// });
+
+
+// // app.use(express.static(__dirname + '/public/css'));
+// // app.use('')
+
+// app.use(express.static(__dirname + '/public/css'));
+
+// // setup another route to listen on /about
+// app.get("/about", function(req,res){
+//   res.sendFile(path.join(__dirname + "/views/about.html"));
+// });
+
+// // setup http server to listen on HTTP_PORT
+// app.listen(HTTP_PORT, onHttpStart);
+
+// app.use(express.static('public'));
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("Xiaochen Wang - 015297153");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
